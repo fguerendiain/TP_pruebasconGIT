bb96f378ce4553a7ca472e0d76ea0e2481750e85
Franco Guerendiain <fguerendiain@agg.local>
2016-03-17 13:50:13 -0300

    se ordena el esqueleto

--- main.c
+++ main.c
@@ -1,8 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-
-
 /*
 Hacer una calculadora. Para ello el programa iniciará y contará con un menú de opciones:
 1. Ingresar 1er operando (A=x)
@@ -23,21 +18,61 @@ que contenga las funciones para realizar las 4 operaciones.
 */
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+
 int main()
 {
+    char seguir='s';
+    int opcion=0;
     float operandoNum1;
     float operandoNum2;
-    float resultado;
-    float resta;
-    float multiplicacion;
-    float divicion;
-    float factorial1;
-    float factorial2;
 
 
+    while(seguir=='s')
+    {
+        printf("1- Ingresar 1er operando (A=x)\n");
+        printf("2- Ingresar 2do operando (B=y)\n");
+        printf("3- Calcular la suma (A+B)\n");
+        printf("4- Calcular la resta (A-B)\n");
+        printf("5- Calcular la division (A/B)\n");
+        printf("6- Calcular la multiplicacion (A*B)\n");
+        printf("7- Calcular el factorial (A!)\n");
+        printf("8- Calcular todas las operacione\n");
+        printf("9- Salir\n");
 
+        printf("Elija una opcion\n")
+        scanf("%d",&opcion);
 
+        switch(opcion)
+        {
+            case 1:
+                printf("Ingrese el 1er operando");
+                scanf("%d",&operandoNum1);
+                break;
+            case 2:
+                printf("Ingrese el 2do operando");
+                scanf("%d",&operandoNum2);
+                break;
+            case 3: //FUNCION SUMA
+                break;
+            case 4: //FUNCION RESTA
+                break;
+            case 5: //FUNCION DIVISION
+                break;
+            case 6: //FUNCION MULTIPLICACION
+                break;
+            case 7: //FUNCION FACTORIAL
+                break;
+            case 8: //TODAS LAS ANTERIORES
+                break;
+            case 9:
+                seguir = 'n';
+                break;
+        }
 
 
     return 0;
 }
+
