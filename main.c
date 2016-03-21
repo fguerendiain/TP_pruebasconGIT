8f1b218314d423b2ed0871ec78773d33d3318769
Franco Guerendiain <fguerendiain33@gmail.com>
2016-03-21 17:45:23 -0300

    Se completan los aspectos basicos del enunciado
    Se definieron variables de preproceso para las opciones del menu
    Se creo la funcion para calcular el factorial de un numero
    para esto se castea a un int, el float ingresado por el usuario
    para el operando 1.
    Se agrega un condicional a la funcion de factorial, para corregir
    el contador al calcular el factorial de 0.
    Se agrega un condicional a la opcion de dividir, para controlar
    que ninguno de los numeros sea menor que 0.
    Se arma la la opcion para realizar todas las operaciones en forma
    simultanea, pero queda a revisar para intentar optimizarla.
    se documenta el codigo para revisar por que al dividir un numero
    por otro mas grande, el resultado es siempre 0.
    
    En este punto el programa cumple con los requisitos minimos del enunciado.

--- main.c
+++ main.c
@@ -30,24 +30,28 @@ que contenga las funciones para realizar las 4 operaciones.
 #define MULTIPLICACION 6
 #define FACTORIAL 7
 #define ALLOPERACIONES 8
+#define EXIT 9
 
 int main()
 {
     char seguir='s';
-    int opcion=0;
-    float operandoNum1=0;
-    float operandoNum2=0;
+
+    int opcion = 0;
+    int operInt1 = 0;
+
+    float operandoNum1 = 0;
+    float operandoNum2 = 0;
     float resultadoSuma;
     float resultadoResta;
     float resultadoDivision;
     float resultadoMultiplicacion;
-    float resultadoFactorial;
+    int resultadoFactorial;
     char limpiaPantalla[7] = "cls";  // "cls" para windows | "clear" para linux
 
     while(seguir=='s')
     {
-        printf("\n1- Ingresar 1er operando (A=x)\n");
-        printf("2- Ingresar 2do operando (B=y)\n");
+        printf("\n1- Ingresar 1er operando (A = %.2f)\n",operandoNum1);
+        printf("2- Ingresar 2do operando (B = %.2f)\n", operandoNum2);
         printf("3- Calcular la suma (A+B)\n");
         printf("4- Calcular la resta (A-B)\n");
         printf("5- Calcular la division (A/B)\n");
@@ -66,38 +70,81 @@ int main()
                 printf("Ingrese el 1er operando\n");
                 scanf("%f",&operandoNum1);
                 break;
+
             case OPER2:
                 system(limpiaPantalla);
                 printf("Ingrese el 2do operando\n");
                 scanf("%f",&operandoNum2);
                 break;
+
             case SUMA:
                 system(limpiaPantalla);
                 resultadoSuma = suma(operandoNum1,operandoNum2);
                 printf("\n%.2f + %.2f = %.2f\n", operandoNum1, operandoNum2, resultadoSuma);
                 break;
+
             case RESTA :
                 system(limpiaPantalla);
                 resultadoResta = resta(operandoNum1, operandoNum2);
                 printf("\n%.2f - %.2f = %.2f\n", operandoNum1, operandoNum2, resultadoResta);
                 break;
+
             case DIVISION:
-                system(limpiaPantalla);
-                resultadoDivision = division(operandoNum1, operandoNum2);
-                printf("\n%.2f : %.2f = %.2f\n", operandoNum1, operandoNum2, resultadoDivision);
-                break;
+                if(operandoNum1<1 || operandoNum2<1){ //CASO DE ERROR DIVISION POR 0
+                    system(limpiaPantalla);
+                    printf("Por favor ingrese valores mayores a 0 para realizar una division\n\n");
+                    break;
+                }else{
+
+                    //A REVISAR: SI UN NUMERO ES DIVIDIDO POR OTRO MAS GRANDE EL RESULTADO DA 0
+
+                    system(limpiaPantalla);
+                    resultadoDivision = division(operandoNum1, operandoNum2);
+                    printf("\n%.2f : %.2f = %.2f\n", operandoNum1, operandoNum2, resultadoDivision);
+                    break;
+                }
+
             case MULTIPLICACION :
                 system(limpiaPantalla);
                 resultadoMultiplicacion = multiplicacion(operandoNum1, operandoNum2);
-                printf("\n%.2f X %.2f = %.2f\n", operandoNum1, operandoNum2, resultadoMultiplicacion);
+                printf("\n%.2f x %.2f = %.2f\n", operandoNum1, operandoNum2, resultadoMultiplicacion);
                 break;
+
             case FACTORIAL :
                 system(limpiaPantalla);
+                operInt1 = (int)operandoNum1;                          /*SE CASTEA EL FLOAT INGRESADO POR EL USUARIO*/
+                resultadoFactorial = factorial(operInt1);              /*A UN INT PARA UTILIZAR EN LA FUNCION*/
+                printf("\n%d! = %d\n", operInt1, resultadoFactorial);
                 break;
+
             case ALLOPERACIONES :
                 system(limpiaPantalla);
-                break;
-            case 9:
+                if(operandoNum1<1 || operandoNum2<1){ //CASO DE ERROR DIVISION POR 0
+                    system(limpiaPantalla);
+                    printf("Por favor ingrese valores mayores a 0 para realizar una division\n\n");
+                    break;
+                }else{
+                    system(limpiaPantalla);
+
+                    resultadoSuma = suma(operandoNum1,operandoNum2);
+                    printf("\n%.2f + %.2f = %.2f\n", operandoNum1, operandoNum2, resultadoSuma);
+
+                    resultadoResta = resta(operandoNum1, operandoNum2);
+                    printf("\n%.2f - %.2f = %.2f\n", operandoNum1, operandoNum2, resultadoResta);
+
+                    resultadoDivision = division(operandoNum1, operandoNum2);
+                    printf("\n%.2f : %.2f = %.2f\n", operandoNum1, operandoNum2, resultadoDivision);
+
+                    resultadoMultiplicacion = multiplicacion(operandoNum1, operandoNum2);
+                    printf("\n%.2f x %.2f = %.2f\n", operandoNum1, operandoNum2, resultadoMultiplicacion);
+
+                    operInt1 = (int)operandoNum1;
+                    resultadoFactorial = factorial(operInt1);
+                    printf("\n%d! = %d\n", operInt1, resultadoFactorial);
+
+                    break;
+                }
+            case EXIT :
                 seguir = 'n';
                 break;
         }
@@ -105,5 +152,6 @@ int main()
     }
     return 0;
 }
+
 
 
--- operaciones.h
+++ operaciones.h
@@ -6,7 +6,7 @@
 #endif // OPERACIONES_H_INCLUDED
 
 
-long suma(long operador1, long operador2){
+long suma(long operador1, long operador2){      //FUNCION PARA CALCULAR SUMA
 
     long resultado;
     resultado = operador1 + operador2;
@@ -14,7 +14,7 @@ long suma(long operador1, long operador2){
     return resultado;
 }
 
-long resta(long operador1, long operador2){
+long resta(long operador1, long operador2){     //FUNCION PARA CALCULAR RESTA
 
     long resultado;
     resultado = operador1 - operador2;
@@ -22,7 +22,7 @@ long resta(long operador1, long operador2){
     return resultado;
 }
 
-long multiplicacion(long operador1, long operador2){
+long multiplicacion(long operador1, long operador2){      //FUNCION PARA CALCULAR MULTIPLICACION
 
     long resultado;
     resultado = operador1 * operador2;
@@ -30,7 +30,7 @@ long multiplicacion(long operador1, long operador2){
     return resultado;
 }
 
-long division(long operador1, long operador2){
+long division(long operador1, long operador2){      //FUNCION PARA CALCULAR DIVISION
 
     long resultado;
     resultado = operador1 / operador2;
@@ -38,4 +38,16 @@ long division(long operador1, long operador2){
     return resultado;
 }
 
+int factorial (int operador1){      //FUNCION PARA CALCULAR FACTORIAL
+
+    int contador;
+    int resultado = 1;
+    if (operador1 == 0){  //CASO DE ERROR CALCULAR FACTORIAL DE 0
+        resultado=0;
+    }
+    for (contador = 1; contador<= operador1 ; contador++){
+        resultado*=contador;
+    }
+    return resultado;
+}
 
