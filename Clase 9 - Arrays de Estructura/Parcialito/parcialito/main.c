4852106252822bc3a21dc6c6188ac1e74fbbd840
Franco Guerendiain <fguerendiain33@gmail.com>
2016-04-13 22:02:41 -0300

    empezar a armar las funciones de la biblioteca

--- Clase 9 - Arrays de Estructura/Parcialito/parcialito/main.c
+++ Clase 9 - Arrays de Estructura/Parcialito/parcialito/main.c
@@ -1,51 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "screenSystemShow.h"
+#include "userInputOutput.h"
+#include "struct.h"
 
-
-typedef struct{
-    int legajo;
-    char nombre[51];
-    char apellido[51];
-    float salario;
-    char sector;
-    long int fechaIngreso;
-    char flagEstado;
-}Empleados;
+#define TOTALEMP 5
 
 int main()
 {
-    Empleados nomina[10000];
+    Empleados nomina[TOTALEMP];
 
     do
     {
-       printMenu()
+       printAwesomeMenu("1- ALTAS\n2- MODIFICAR\n3- BAJA\n4-INFORMAR\n5-LISTAR\n\n");
        userInput=getUserInput();
 
        switch(userInput);
 
         case 1 ://ALTA
-        numeroUltimoLegajo(array, lenght); //recorre buscando el maximo y suma 1 numero de legajo
+/*      numeroUltimoLegajo(array, lenght); //recorre buscando el maximo y suma 1 numero de legajo
         posicionLibre(array, lenght); //recorre buscando el estado libre
         cargaDatos(array, lenght); //solicita datos al usuario y los carga en posicion libre
-
-        /*DESARROLLAR PARA BIBLIOTECA*/
-        getInt(*userImput,"mensaje", min, max, "mensaje Error");
-        getfloat(*userImput,"mensaje", min, max, "mensaje Error");
-        getChar(*userImput,"mensaje", "si", "no", "mensaje Error");
-        getString(*userImput,"mensaje", cantMinLetras,cantMaxLetras,"MensajeError");
-        printMenu("");
-        /*devuelven si pudo o no realizar la tarea y guarda lo ingresado en userInput*/
-
-        ; //recorre el array nomina y busca el primer miembro estado en 0, devolviendo el indice
-        solicitarIngreso(empleado); //solicita al usuario ingresar los miembros de la estructura en la posicion pasada como parametro
+*/
         break;
 
         case 2 : // MODIFICAR
-        controlState=chekStatus(); //recorre el array nomina y busca por legajo el ingresado por el usuario revisa si el miembro estado se encuentra en 1 o 0 y arroja un mensaje indicando si existe o no
-        if(controlState == 0)
-            {
-                solicitarIngreso(empleado); //solicita al usuario ingresar los miembros de la estructura en la posicion pasada como parametro
-            }
+        break;
 
     }while(opcion);
 
