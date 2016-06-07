26ced1d86b41bacb54922ebaa9f7cb6099efad83
Franco Guerendiain <fguerendiain33@gmail.com>
2016-06-07 05:13:21 -0300

    inicio cascara entrega

---
+++ TP4 - fguerendiain 1F/TP4_usingArrayList/main.c
@@ -0,0 +1,53 @@
+/***************************************************************************************************
+****************************************************************************************************
+**
+**  LABORATORIO I   |  TP 4: ARRAYLIST
+**
+**  ALUMNO: GUERENDIAIN, FRANCO
+**  DIVISION: 1ºF
+**  LEGAJO: 104333
+**  CICLO LECTIVO: 2016
+**
+****************************************************************************************************
+****************************************************************************************************
+**
+** *1.3 Condiciones de Aprobación
+**
+** Se deberá entregar un proyecto de código ANSI C el cual estará compuesto de un programa
+** que utilice la biblioteca ArrayList en su totalidad, el mismo deberá contar como mínimo con
+** las funciones obligatorias, todas ellas con su respectiva documentación, y un programa que
+** utilice de manera integral la biblioteca. En la fecha del segundo parcial se realizará una
+** defensa oral del trabajo por parte del alumno a efectos de determinar si se encuentra o no en
+** condiciones de rendir examen final.
+--
+** 1.4 Funciones Obligatorias
+**
+** • al_newArrayList
+** • al_add
+** • al_deleteArrayList
+** • al_len
+** • al_get
+** • al_contains
+** • al_set
+** • al_remove
+** • al_clear
+** • al_push
+** • al_indexOf
+** • al_isEmpty
+** • al_pop
+**
+****************************************************************************************************
+***************************************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "screenSystemShow.h"
+#include "userInputOutput.h"
+
+
+int main()
+{
+
+
+    return 0;
+}
